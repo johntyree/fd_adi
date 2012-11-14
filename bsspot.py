fb85829803b8c3133797797abea5d37a3925aae5
John Tyree <johntyree@gmail.com>
2012-11-14 18:54:10 +0100

    Further optimized crank scheme. Approaching max?

--- bsspot.py
+++ bsspot.py
@@ -58,7 +58,7 @@ k = 100.0
 r = 0.03
 t = 1
 v0 = 0.04
-dt = 1 / 200.0
+dt = 1 / 100.0
 
 kappa = 1
 theta = 0.04
@@ -72,10 +72,10 @@ rate_Spot_Var = 0.5  # Proportion to solve in the var step
 spot_max = 1500.0
 var_max = 13.0
 
-nspots = 200
-nvols = 200
+nspots = 100
+nvols = 100
 
-spotdensity = 10.0  # infinity is linear?
+spotdensity = 7.0  # infinity is linear?
 varexp = 4
 
 spots = utils.sinh_space(k, spot_max, spotdensity, nspots)
@@ -93,7 +93,8 @@ trimv = (0.01 < vars) & (vars < 1)  # v0*2.0)
 # trims = slice(None)
 # trimv = slice(None)
 
-up_or_down_spot = 'up'
+# Does better without upwinding here
+up_or_down_spot = ''
 up_or_down_var = 'down'
 flip_idx_var = min(find(vars > theta))
 flip_idx_spot = 2
@@ -114,8 +115,8 @@ def init(spots, nvols, k):
     return tile(np.maximum(0, spots - k), (nvols, 1)).T
 
 
-Vi = init(spots, nvols, k)
-V = np.copy(Vi)
+V_init = init(spots, nvols, k)
+V = np.copy(V_init)
 bs, delta = [x for x in bs_call_delta(spots[:, newaxis], k, r,
                                       np.sqrt(vars)[newaxis, :], t)]
 utils.tic("Heston Analytical:")
@@ -256,22 +257,26 @@ def force_boundary(V, values=None, t=None):
 
 def impl(V, L1, R1x, L2, R2x, dt, n, crumbs=[], callback=None):
     V = V.copy()
-    L1i = [x.copy() for x in L1]
-    R1 = [x.copy() for x in R1x]
-    L2i = [x.copy() for x in L2]
-    R2 = [x.copy() for x in R2x]
+
+    L1i = flatten_tensor(L1)
+    R1 = np.array(R1x).T
+
+    L2i = flatten_tensor(L2)
+    R2 = np.array(R2x)
 
     m = 2
 
     # L  = (As + Ass - r*np.eye(nspots))*-dt + np.eye(nspots)
-    for j in xrange(nvols):
-        L1i[j].data *= -dt
-        L1i[j].data[m, :] += 1
-        R1[j] *= dt
-    for i in xrange(nspots):
-        L2i[i].data *= -dt
-        L2i[i].data[m, :] += 1
-        R2[i] *= dt
+    L1i.data *= -dt
+    L1i.data[m, :] += 1
+    R1 *= dt
+
+    L2i.data *= -dt
+    L2i.data[m, :] += 1
+    R2 *= dt
+
+    offsets1 = (abs(min(L1i.offsets)), abs(max(L1i.offsets)))
+    offsets2 = (abs(min(L2i.offsets)), abs(max(L2i.offsets)))
 
     print_step = max(1, int(n / 10))
     to_percent = 100.0 / n
@@ -283,25 +288,21 @@ def impl(V, L1, R1x, L2, R2x, dt, n, crumbs=[], callback=None):
                 return crumbs
             print int(k * to_percent),
         if callback is not None:
-            callback(V, (n - k) * dt)
-        for j in xrange(nvols):
-            V[:, j] = spl.solve_banded(
-                (abs(min(L1i[j].offsets)), abs(max(L1i[j].offsets))),
-                L1i[j].data, V[:, j] + R1[j], overwrite_b=True)
-        for i in xrange(nspots):
-            V[i, :] = spl.solve_banded(
-                (abs(min(L2i[i].offsets)), abs(max(L2i[i].offsets))),
-                L2i[i].data, V[i, :] + R2[i], overwrite_b=True)
-        crumbs.append(V.copy())
+            callback(V, ((n - k) * dt))
+        V = spl.solve_banded(offsets2, L2i.data,
+                             (V + R2).flat, overwrite_b=True).reshape(V.shape)
+        V = spl.solve_banded(offsets1, L1i.data,
+                             (V + R1).T.flat, overwrite_b=True).reshape(V.shape[::-1]).T
+    crumbs.append(V.copy())
     utils.toc()
     return crumbs
-
 
 
 def flatten_tensor(mats):
     diags = np.hstack([x.data for x in mats])
     flatmat = sps.dia_matrix((diags, mats[0].offsets), shape=(diags.shape[1], diags.shape[1]))
     return flatmat
+
 
 def crank(V, L1, R1x, L2, R2x, dt, n, crumbs=[], callback=None):
     V = V.copy()
@@ -336,6 +337,9 @@ def crank(V, L1, R1x, L2, R2x, dt, n, crumbs=[], callback=None):
     print_step = max(1, int(n / 10))
     to_percent = 100.0 / n
     utils.tic("Crank:")
+    R = R1 + R2
+    normal_shape = V.shape
+    transposed_shape = normal_shape[::-1]
     for k in xrange(n):
         if not k % print_step:
             if isnan(V).any():
@@ -344,12 +348,10 @@ def crank(V, L1, R1x, L2, R2x, dt, n, crumbs=[], callback=None):
             print int(k * to_percent),
         if callback is not None:
             callback(V, ((n - k) * dt))
-        V = (L1e.dot(V.T.flat).reshape(V.shape[::-1]).T) + R1
-        V = spl.solve_banded(offsets2, L2i.data,
-                             (V + R2).flat, overwrite_b=True).reshape(V.shape)
-        V = L2e.dot(V.flat).reshape(V.shape) + R2
-        V = spl.solve_banded(offsets1, L1i.data,
-                             (V + R1).T.flat, overwrite_b=True).reshape(V.shape[::-1]).T
+        V = (L2e.dot(V.flat).reshape(normal_shape) + R).T
+        V = spl.solve_banded(offsets1, L1i.data, V.flat, overwrite_b=True)
+        V = (L1e.dot(V).reshape(transposed_shape).T) + R
+        V = spl.solve_banded(offsets2, L2i.data, V.flat, overwrite_b=True).reshape(normal_shape)
         crumbs.append(V.copy())
     utils.toc()
     return crumbs
@@ -402,24 +404,24 @@ vis = lambda V=V, p=p2: p(V, 0, spots, vars, 0, "")
 vis2 = lambda V=V, p=p2: p(tr(V), 0, spots[trims], vars[trimv], 0, "")
 
 
-# Vs = impl(Vi, L1_, R1_, L2_, R2_,
-          # dt, int(t / dt), crumbs=[Vi]
+# Vs = impl(V_init, L1_, R1_, L2_, R2_,
+          # dt, int(t / dt), crumbs=[V_init]
           # # , callback=lambda v, t: force_boundary(v, hs)
           # )
-# Vi = Vs[0]
+# Vi = Vs[-1]
 # print tr(Vi)[ids, idv] - tr(hs)[ids, idv]
 
-# Vs = crank(Vi, L1_, R1_, L2_, R2_,
-           # dt, int(t / dt), crumbs=[Vi]
-           # # , callback=lambda v, t: force_boundary(v, hs)
-           # )
-# Vc = Vs[-1]
-# print tr(Vc)[ids, idv] - tr(hs)[ids, idv]
+Vs = crank(V_init, L1_, R1_, L2_, R2_,
+           dt, int(t / dt), crumbs=[V_init]
+           # , callback=lambda v, t: force_boundary(v, hs)
+           )
+Vc = Vs[-1]
+print tr(Vc)[ids, idv] - tr(hs)[ids, idv]
 
 ## Rannacher smoothing to damp oscilations at the discontinuity
 smoothing_steps = 2
-Vs = impl(Vi, L1_, R1_, L2_, R2_,
-          dt, smoothing_steps, crumbs=[Vi]
+Vs = impl(V_init, L1_, R1_, L2_, R2_,
+          dt, smoothing_steps, crumbs=[V_init]
           # , callback=lambda v, t: force_boundary(v, hs)
           )
 Vs.extend(crank(Vs[-1], L1_, R1_, L2_, R2_,
@@ -432,11 +434,11 @@ print tr(Vr)[ids, idv] - tr(hs)[ids, idv]
 
 ion()
 # p(tr(Vi), tr(hs), spots[trims], vars[trimv], 1, "impl")
-# p(tr(Vc), tr(hs), spots[trims], vars[trimv], 2, "crank")
+p(tr(Vc), tr(hs), spots[trims], vars[trimv], 2, "crank")
 p(tr(Vr), tr(hs), spots[trims], vars[trimv], 3, "smooth")
 ioff()
 show()
-# p(Vi, hs, spots, vars, 1, "impl")
+# p(V_init, hs, spots, vars, 1, "impl")
 # p(Vc, hs, spots, vars, 1, "crank")
 # p(Vr, hs, spots, vars, 1, "smooth")
 
