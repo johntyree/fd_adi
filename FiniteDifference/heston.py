Fix boundary condition for second derivative in Heston.

FiniteDifference/heston.py
@@ -332,8 +332,9 @@ class HestonFiniteDifferenceEngine(FiniteDifferenceEngineADI):
                             (None, lambda t, *dim: None)
                             # (0, lambda t, *dim: dim[0])
                             ),
-                            # # Free boundary
-                    (1,1) : ((None, lambda t, *dim: None),
+                            # We know from the PDE that this will be 0 because
+                            # the vol is 0 at the low boundary
+                    (1,1) : ((1, lambda t, *dim: 0),
                             # D intrinsic value at high variance
                             # (0, lambda t, *dim: np.exp(-option.interest_rate.value * t) * np.maximum(0.0, np.exp(dim[0])-option.strike))),
                             (None, lambda t, *dim: None)

FiniteDifference/test.py
@@ -184,7 +184,7 @@ class FiniteDifferenceEngineADI_test(unittest.TestCase):
                         (0 if dirichlet_v else 1, lambda t, *dim: np.maximum(0.0, dim[0]-k))
                         ),
                         # # Free boundary
-                (1,1) : ((None, lambda *x: None),
+                (1,1) : ((1, lambda *x: 0),
                         # D intrinsic value at high variance
                         # (None, lambda *x: None)
                         (0 if dirichlet_v else 1, lambda t, *dim: np.maximum(0.0, dim[0]-k))