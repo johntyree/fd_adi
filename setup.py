abe0e04983d8fcd671c30491caa8e115a92fc341
John Tyree <johntyree@gmail.com>
2013-02-14 23:18:14 +0100

    Use -O because *damn* there's some crazy warnings with -O2.

--- setup.py
+++ setup.py
@@ -72,7 +72,7 @@ for m in cython_modules:
     m.libraries = ['cudart', 'cusparse']
     m.include_dirs += thrust + ["FiniteDifference", numpy_include, CUDA['include']]
     m.runtime_library_dirs = [CUDA['lib64']]
-    both_args = ['-O2']
+    both_args = ['-O']
     embedded_gcc_args = ["-g", "-Wall", "-fPIC"] + both_args
     m.extra_compile_args = {}
     m.extra_compile_args['gcc'] = ["-Wextra", "-pedantic", "-std=c++0x"] + embedded_gcc_args
