43d03bc99115dfb76cf670bb9a9a2c201cf18fd5
John Tyree <johntyree@gmail.com>
2013-02-14 17:34:29 +0100

    Add debug information to gcc flags.

--- setup.py
+++ setup.py
@@ -73,7 +73,7 @@ for m in cython_modules:
     m.include_dirs += thrust + ["FiniteDifference", numpy_include, CUDA['include']]
     m.runtime_library_dirs = [CUDA['lib64']]
     both_args = []#['-O0']
-    embedded_gcc_args = ["-Wall", "-fPIC"] + both_args
+    embedded_gcc_args = ["-g", "-Wall", "-fPIC"] + both_args
     m.extra_compile_args = {}
     m.extra_compile_args['gcc'] = ["-Wextra", "-pedantic", "-std=c++0x"] + embedded_gcc_args
     m.extra_compile_args['nvcc'] = ['-arch='+NVCC_ARCH, '--ptxas-options=-v', '-c', '--compiler-options="%s"' % ' '.join(embedded_gcc_args)] + both_args
