0f7be69927a4d1ae95b558798211c4ee833cb4c1
John Tyree <johntyree@gmail.com>
2013-03-17 04:52:58 +0100

    Fix autotest *again*

--- autotest.sh
+++ autotest.sh
@@ -2,35 +2,23 @@
 
 onmodify () {
     TARGET=${1:-.};
-    [[ $ALL ]] && rm -f .noseids
+    remove_ids
     while inotifywait -qq -r -e close_write,moved_to,move_self $TARGET; do
         sleep 0.5;
         build && run;
     done
 }
 
-while getopts ":acfb" opt; do
-    case $opt in
-        a)
-            ALL="remove_ids"
-            ;;
-        c)
-            CCC="force_rebuild;"
-            ;;
-        f)
-            failed="--failed"
-            ;;
-        b)
-            $CCC
-            python setup.py build_ext --inplace 2>&1
-            exit
-            ;;
-    esac
-done
-shift $(($OPTIND-1))
+remove_ids () {
+    [[ $ALL ]] && rm -f .noseids
+}
+
+force_rebuild () {
+    (for i in FiniteDifference/*.pyx; do rm ${i%%.pyx}.cpp; done)
+}
 
 build () {
-    [[ $CCC ]] && (for i in FiniteDifference/*.pyx; do rm ${i%%.pyx}.cpp; done)
+    [[ $CCC ]] && force_rebuild
     set -o pipefail
     python setup.py build_ext --inplace 2>&1 | grep -Ei --color -e '' -e error
 }
@@ -38,5 +26,25 @@ build () {
 run () {
     nosetests $failed --rednose --verbosity=3 --with-id $@ || echo -ne '\a'
 }
+
+
+while getopts ":acfb" opt; do
+    case $opt in
+        a)
+            ALL="remove_ids"
+            ;;
+        c)
+            CCC="force_rebuild"
+            ;;
+        f)
+            failed="--failed"
+            ;;
+        b)
+            build
+            exit
+            ;;
+    esac
+done
+shift $(($OPTIND-1))
 
 onmodify .
