10376795528e86e28e2be7fb973b9bcda38e58b8
John Tyree <johntyree@gmail.com>
2013-03-17 02:03:46 +0100

    Move autotest into build and run functions

--- autotest.sh
+++ autotest.sh
@@ -2,30 +2,11 @@
 
 onmodify () {
     TARGET=${1:-.};
-    shift;
-    CMD="$@";
-    echo "$TARGET" "$CMD";
-    ( if [ -f onmodify.sh ]; then
-        . onmodify.sh;
-    fi;
+    [[ $ALL ]] && rm -f .noseids
     while inotifywait -qq -r -e close_write,moved_to,move_self $TARGET; do
         sleep 0.5;
-        if [ "$CMD" ]; then
-            bash -c "$CMD";
-        else
-            build && run;
-        fi;
-        echo;
-    done )
-}
-
-function force_rebuild() {
-    for i in FiniteDifference/*.pyx; do
-        rm ${i%%.pyx}.cpp;
+        build && run;
     done
-}
-function remove_ids() {
-    rm .noseids
 }
 
 while getopts ":acfb" opt; do
@@ -34,7 +15,7 @@ while getopts ":acfb" opt; do
             ALL="remove_ids"
             ;;
         c)
-            CCC="force_rebuild"
+            CCC="force_rebuild;"
             ;;
         f)
             failed="--failed"
@@ -48,9 +29,14 @@ while getopts ":acfb" opt; do
 done
 shift $(($OPTIND-1))
 
-CMD="$ALL $CCC (set -o pipefail; python setup.py build_ext --inplace 2>&1 | grep -Ei --color -e '' -e error) \
-    && nosetests $failed --rednose --verbosity=3 --with-id $@ \
-    || echo -ne '\a'
-"
+build () {
+    [[ $CCC ]] && (for i in FiniteDifference/*.pyx; do rm ${i%%.pyx}.cpp; done)
+    set -o pipefail
+    python setup.py build_ext --inplace 2>&1 | grep -Ei --color -e '' -e error
+}
 
-onmodify . $CMD
+run () {
+    nosetests $failed --rednose --verbosity=3 --with-id $@ || echo -ne '\a'
+}
+
+onmodify .
