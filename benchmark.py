541b163d372ef525882d7d32339efa24025a5067
John Tyree <johntyree@gmail.com>
2013-03-28 02:02:45 +0100

    Cleanup benchmark with argparse and new FDEGPU.

--- benchmark.py
+++ benchmark.py
@@ -3,18 +3,20 @@
 """Benchmarking the FiniteDifferenceEngine."""
 
 import sys
+import argparse
 
 from FiniteDifference import utils
 
 from FiniteDifference.Grid import Grid
 from FiniteDifference.FiniteDifferenceEngineGPU import FiniteDifferenceEngineADI as FDE_ADI_GPU
+from FiniteDifference.FiniteDifferenceEngineGPU import HestonFiniteDifferenceEngine as HestonFDEGPU
 from FiniteDifference.heston import HestonOption, HestonBarrierOption, hs_call_vector, HestonFiniteDifferenceEngine
 from FiniteDifference.blackscholes import BlackScholesOption
 
 from FiniteDifference.visualize import fp
 
 Opt = HestonOption
-Opt = HestonBarrierOption
+# Opt = HestonBarrierOption
 
 DefaultHeston = Opt( spot=100
                  , strike=100
@@ -69,12 +71,20 @@ print H
 # trimv = slice(None)
 
 
-def create(nspots=30, nvols=30):
+def create_cpu(nspots=30, nvols=30):
     F = HestonFiniteDifferenceEngine(H, nspots=nspots,
                                         nvols=nvols, spotdensity=10, varexp=4,
                                         var_max=12, verbose=False)
     F.init()
     F.operators[1].diagonalize()
+    return F
+
+def create_gpu(nspots=30, nvols=30):
+    F = HestonFDEGPU(H, nspots=nspots,
+                     nvols=nvols, spotdensity=10, varexp=4,
+                     var_max=12, verbose=False)
+    F.make_operator_templates()
+    F.scale_and_combine_operators()
     return F
 
 
@@ -106,41 +116,37 @@ def run(dt, F=None, func=None, initial=None):
     return Vs
 
 
+def read_args():
+    parser = argparse.ArgumentParser(description="Run a benchmark")
+    parser.add_argument('-s', '--scheme', metavar='scheme', choices="i,d,hv,s".split(','), default='hv')
+    parser.add_argument('-n', default=252, metavar='int', dest='n', type=int, help="Number of time steps")
+    parser.add_argument('-ns', '--nspots', default=200, metavar='int', type=int, help="Number of spots")
+    parser.add_argument('-nv', '--nvols', default=200, metavar='int', type=int, help="Number of volatilities")
+    parser.add_argument('-gpu', action='store_const', dest='gpu', default=False, const=True)
+    parser.add_argument('-cpu', action='store_const', dest='cpu', default=False, const=True)
+    parser.add_argument('-mc', '--monte-carlo', metavar='int', type=int, dest='npaths', default=0)
+    return parser.parse_args()
+
+
 def main():
-    if len(sys.argv) > 1:
-        func = sys.argv[1]
-    else:
-        func=None
+    opt = read_args()
 
-    if len(sys.argv) > 2:
-        nspots = int(sys.argv[2])
-    else:
-        nspots = 200
+    print opt.scheme, opt.nspots, opt.nvols, opt.n
 
-    if len(sys.argv) > 3:
-        nvols = int(sys.argv[3])
-    else:
-        nvols = 200
-
-    if len(sys.argv) > 4:
-        dt = float(sys.argv[4])
-    else:
-        dt = 1.0 / 252
-
-    print func, nspots, nvols, dt
-
-    F = create(nspots=nspots, nvols=nvols)
-    idx = F.idx
-    FG = FDE_ADI_GPU()
-    FG.from_host_FiniteDifferenceEngine(F)
-    # mc = F.option.monte_carlo()
     print
-    # print run(dt, F, func)[idx]
-    # F.grid.reset()
-    print run(dt, FG,func,F.grid.domain[0])[idx],
-    # print "Estimate:", mc['expected'], "+-", mc['error']
-    # print F.option.analytical
-    print
+    if opt.cpu:
+        F = create_cpu(nspots=opt.nspots, nvols=opt.nvols)
+        idx = F.idx
+        if opt.npaths:
+            mc = F.option.monte_carlo()
+            print mc
+        print run(1.0/opt.n, F, opt.scheme)[idx]
+        F.grid.reset()
+    if opt.gpu:
+        FG = create_gpu(nspots=opt.nspots, nvols=opt.nvols)
+        idx = FG.idx
+        print run(1.0/opt.n, FG, opt.scheme, F.grid.domain[0])[idx],
+        print
 
 if __name__ == '__main__':
     main()
