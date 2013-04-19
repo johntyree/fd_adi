c00e0c99767146383646c63dbb0bde290b1ec257
John Tyree <johntyree@gmail.com>
2013-04-19 15:11:41 +0200

    Add hard copies of debug functions backtrace and filter.

--- FiniteDifference/backtrace.c
+++ FiniteDifference/backtrace.c
@@ -1,1 +1,71 @@
-/home/tyree/projects/QDC/backtrace.c
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+#include <execinfo.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <assert.h>
+
+#include "filter.h"
+
+#ifndef min
+#define min(a,b) ((a < b) ? (a) : (b))
+#endif
+
+char* demangle2(char *bt) {
+    char *output = NULL;
+    char * const argv[] = {"/usr/bin/c++filt", "c++filt", NULL};
+    int argc = 3;
+    filter(&output, 0, bt, strlen(bt), argc, argv);
+    return output;
+}
+
+
+size_t demangle(char *dest, char *bt, size_t n) {
+    assert(dest);
+    assert(bt);
+    char *output = demangle2(bt);
+    size_t s = strlen(output)+1;
+    n = s > n ? n : s;
+    memcpy(dest, output, n);
+    free(output);
+    return n;
+}
+
+char* get_backtrace(char *full, size_t size) {
+    int j, nptrs;
+    void *buffer[100];
+    char **strings;
+
+    nptrs = backtrace(buffer, size);
+    // printf("backtrace() returned %d addresses\n", nptrs);
+
+    /* The call backtrace_symbols_fd(buffer, nptrs, STDOUT_FILENO)
+        would produce similar output to the following: */
+
+    strings = backtrace_symbols(buffer, nptrs);
+    if (strings == NULL) {
+        perror("backtrace_symbols() failed.");
+        exit(EXIT_FAILURE);
+    }
+
+    full[0] = '\0';
+    for (j = 1; j < min(10, nptrs); j++) {
+        strcat(full, strings[j]);
+        strcat(full, "\n");
+    }
+
+    free(strings);
+    char buf[size];
+    demangle(buf, full, size);
+    memcpy(full, buf, size);
+    return full;
+}
+
+#ifdef __cplusplus
+} /* extern C */
+#endif
--- FiniteDifference/backtrace.h
+++ FiniteDifference/backtrace.h
@@ -1,1 +1,21 @@
-/home/tyree/projects/QDC/backtrace.h
+#ifndef BACKTRACE_H
+#define BACKTRACE_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Demangle C++ symbols and return ptr to entire result, unbounded. (scary!) */
+char* demangle2(char *bt);
+
+/* Demangle C++ symbols in *bt and save n bytes of the result in *dest. */
+size_t demangle(char *dest, char *bt, size_t n);
+
+/* Write a backtrace of depth size into *buf, return buf */
+char* get_backtrace(char *buf, size_t size);
+
+#ifdef __cplusplus
+} /* extern C */
+#endif
+
+#endif /* end of include guard */
